5c0743911266feba05876b26154d57107d56903f
junland <unlandj2012@gmail.com>
2019-09-24 20:34:24 -0500

    Move some stuff and clean up.

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,7 +9,7 @@ REVDEPCONF=/etc/revdep.conf
 
 mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${PORT_DIR},${REVDEPD}}
 install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist pkglibdepends scratch pkgbase ${DESTDIR}${BINDIR}
-install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
+install -m644 conf/scratchpkg.conf conf/scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
 install -dm777 ${DESTDIR}${CACHE_DIR}/{packages,sources,log,work}
-install -m644 revdep.conf ${DESTDIR}${REVDEPCONF}
+install -m644 conf/revdep.conf ${DESTDIR}${REVDEPCONF}
